ea18a080d7ffbe417d28fa4911f68a383744e49d
chanrefuerzoofficial <chanrefuerzoofficial@gmail.com>
2023-05-15 15:28:50 +0800

    first commit 1

---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,40 @@
+import HText from "@/shared/HText";
+import Logo from "../../assets/Logo.png";
+
+type Props = {};
+
+export default function footer({}: Props) {
+  return (
+    <footer className="bg-red-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img src={Logo} alt="logo" />
+          <p className="my-5">
+            Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
+            purus et arcu massa dictum condimentum. Augue scelerisque iaculis
+            orci ut habitant laoreet. Iaculis tristique.
+          </p>
+          <p>© Evogym All Rights Reserved.</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold"> Links</h4>
+          <p className="my-5">
+            Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
+          </p>
+          <p>
+            Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
+          </p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold"> Contact Us</h4>
+          <p className="my-5">
+            Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
+          </p>
+          <p>
+            Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
+          </p>
+        </div>{" "}
+      </div>
+    </footer>
+  );
+}
---
+++ src/shared/ActionButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { SelectedPage } from "./types";
+
+type Props = {
+  children: React.ReactNode;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+export default function ActionButton({ children, setSelectedPage }: Props) {
+  return (
+    <AnchorLink
+      className="rounded-md bg-yellow-500 px-10 py-2 hover:bg-red-500 hover:text-white"
+      onClick={() => setSelectedPage(SelectedPage.ConctactUs)}
+      href={`#${SelectedPage.ConctactUs}`}
+    >
+      {children}
+    </AnchorLink>
+  );
+}
